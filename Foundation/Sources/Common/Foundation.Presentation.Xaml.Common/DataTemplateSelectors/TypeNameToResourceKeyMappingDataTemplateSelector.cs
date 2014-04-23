a5217d182d568a54ae44f9bf37c5c61c5b41ad65
squaredinfinity <jarek@squaredinfinity.com>
2014-04-23 14:41:40 +0100

    typanametoresourcekeymappingdatatemplateselectoradded

---
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/DataTemplateSelectors/TypeNameToResourceKeyMappingDataTemplateSelector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using SquaredInfinity.Foundation.Extensions;
+using System.Windows.Controls.Primitives;
+
+namespace SquaredInfinity.Foundation.Presentation.DataTemplateSelectors
+{
+    public class TypeNameToResourceKeyMappingDataTemplateSelector : DataTemplateSelector
+    {
+        public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            if (item == null)
+                return null;
+
+            var itemType = item.GetType();
+
+            var result = (DataTemplate)null;
+
+            // first try to find in parent
+
+            var container_frameworkElement = container as FrameworkElement;
+            if (container_frameworkElement != null && TryFindDataTemplate(container_frameworkElement.Resources, itemType, out result))
+                    return result;
+
+            // first try to find in parent selector
+
+            var selector_frameworkElement = container.FindVisualParent<Selector>();
+            if (selector_frameworkElement != null && TryFindDataTemplate(selector_frameworkElement.Resources, itemType, out result))
+                return result;
+
+            // then try globally
+
+            if (Application.Current != null && TryFindDataTemplate(Application.Current.Resources, itemType, out result))
+                return result;             
+
+            return null;
+        }
+
+        bool TryFindDataTemplate(ResourceDictionary dict, Type type, out DataTemplate dataTemplate)
+        {
+            dataTemplate = dict[type.FullName] as DataTemplate;
+            if (dataTemplate != null)
+                return true;
+
+            dataTemplate = dict[type.Name] as DataTemplate;
+            if (dataTemplate != null)
+                return true;
+
+            if(type.Name.EndsWith("ViewModel"))
+            {
+                dataTemplate = dict[type.Name.Substring(0, type.Name.Length - "ViewModel".Length)] as DataTemplate;
+                if (dataTemplate != null)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Foundation/Sources/DotNet45/Foundation.Presentation.Xaml.DotNet45/Properties/AssemblyInfo.cs
+++ Foundation/Sources/DotNet45/Foundation.Presentation.Xaml.DotNet45/Properties/AssemblyInfo.cs
@@ -39,5 +39,6 @@ using System.Windows.Markup;
 //[assembly: XmlnsDefinition(@"http://schemas.squaredinfinity.com/foundation/behaviors", "SquaredInfinity.Foundation.Presentation.Behaviors")]
 [assembly: XmlnsDefinition(@"http://schemas.squaredinfinity.com/foundation/converters", "SquaredInfinity.Foundation.Presentation.Converters")]
 [assembly: XmlnsDefinition(@"http://schemas.squaredinfinity.com/foundation/commands", "SquaredInfinity.Foundation.Presentation.Commands")]
+[assembly: XmlnsDefinition(@"http://schemas.squaredinfinity.com/foundation/datatemplateselectors", "SquaredInfinity.Foundation.Presentation.DataTemplateSelectors")]
 [assembly: XmlnsDefinition(@"http://schemas.squaredinfinity.com/foundation/decorators", "SquaredInfinity.Foundation.Presentation.Decorators")]
 [assembly: XmlnsDefinition(@"http://schemas.microsoft.com/winfx/2006/xaml/presentation", "SquaredInfinity.Foundation.Presentation.MarkupExtensions")]
