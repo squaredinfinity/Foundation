7f518896a0c5e2e3858fa70032a5787acf4f5bb9
squaredinfinity <jarek@squaredinfinity.com>
2014-05-15 06:37:43 +0100

    added previously untracked files

---
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/ViewModels/ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SquaredInfinity.Foundation.Presentation.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SquaredInfinity.Foundation.Presentation.ViewModels
+{
+    /// <summary>
+    /// Base class for all viewmodels
+    /// </summary>
+    public abstract partial class ViewModel : NotifyPropertyChangedObject, IViewModel
+    {
+        ViewModelState _state = ViewModelStates.Initialising;
+        /// <summary>
+        /// Gets or sets the state of a ViewModel
+        /// </summary>
+        /// <value>The state.</value>
+        public ViewModelState State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                RaisePropertyChanged(() => State);
+
+            }
+        }
+    }
+}
