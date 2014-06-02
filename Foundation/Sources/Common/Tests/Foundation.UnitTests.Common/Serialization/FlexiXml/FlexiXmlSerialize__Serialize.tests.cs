bc03e78f751f986e44f1fe9550f489c2a4c437c5
squaredinfinity <jarek@squaredinfinity.com>
2014-06-02 17:38:00 +0100

    tests added

--- Foundation/Sources/Common/Tests/Foundation.UnitTests.Common/Serialization/FlexiXml/FlexiXmlSerialize__Serialize.tests.cs
+++ Foundation/Sources/Common/Tests/Foundation.UnitTests.Common/Serialization/FlexiXml/FlexiXmlSerialize__Serialize.tests.cs
@@ -39,5 +39,34 @@ namespace SquaredInfinity.Foundation.Serialization.FlexiXml
 
             Assert.IsTrue(xml.Descendants().Count() == 3, "Not all collection items have been serialized");
         }
+
+        [TestMethod]
+        public void CollectionWithNullableValueTypeItems__CanSerialize()
+        {
+            var list = new List<int?>();
+            list.Add(null);
+            list.Add(1);
+            list.Add(2);
+            list.Add(null);
+            list.Add(4);
+            list.Add(null);
+
+            var s = new FlexiXml.FlexiXmlSerializer();
+
+            var xml = s.Serialize(list);
+
+            Assert.IsNotNull(xml);
+
+            var deserialized = s.Deserialize<List<int?>>(xml);
+
+            Assert.AreEqual(6, deserialized.Count);
+            Assert.IsNull(deserialized[0]);
+            Assert.IsNull(deserialized[3]);
+            Assert.IsNull(deserialized[5]);
+
+            Assert.AreEqual(1, deserialized[1]);
+            Assert.AreEqual(2, deserialized[2]);
+            Assert.AreEqual(4, deserialized[4]);
+        }
     }
 }
