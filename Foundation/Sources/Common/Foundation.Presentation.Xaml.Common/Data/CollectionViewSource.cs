9c2862e7182c54f4ef9c74ca0a3a124b660e54c0
squaredinfinity <jarek@squaredinfinity.com>
2014-06-11 11:59:29 +0100

    CollectionViewSource - refreshtrigger property added

--- Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Data/CollectionViewSource.cs
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Data/CollectionViewSource.cs
@@ -16,12 +16,43 @@ namespace SquaredInfinity.Foundation.Presentation.Data
         }
 
         public static readonly DependencyProperty FilterPredicateProperty =
-            DependencyProperty.Register("FilterPredicate", typeof(Predicate<object>), typeof(CollectionViewSource), new PropertyMetadata(null));
+            DependencyProperty.Register(
+            "FilterPredicate",
+            typeof(Predicate<object>),
+            typeof(CollectionViewSource), 
+            new PropertyMetadata(null));
+
+
+        public object RefreshTriggerProperty
+        {
+            get { return (object)GetValue(RefreshTriggerPropertyProperty); }
+            set { SetValue(RefreshTriggerPropertyProperty, value); }
+        }
+
+        public static readonly DependencyProperty RefreshTriggerPropertyProperty =
+            DependencyProperty.Register(
+            "RefreshTriggerProperty",
+            typeof(object),
+            typeof(CollectionViewSource),
+            new PropertyMetadata(null, OnRefreshTriggerPropertyChanged));
+
+        static void OnRefreshTriggerPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var cvs = d as CollectionViewSource;
+
+            cvs.View.Refresh();
+        }
+      
 
 
         public CollectionViewSource()
         {
             this.Filter += CollectionViewSource_Filter;
+        }
+
+        protected override void OnSourceChanged(object oldSource, object newSource)
+        {
+            base.OnSourceChanged(oldSource, newSource);
         }
 
         void CollectionViewSource_Filter(object sender, FilterEventArgs e)
