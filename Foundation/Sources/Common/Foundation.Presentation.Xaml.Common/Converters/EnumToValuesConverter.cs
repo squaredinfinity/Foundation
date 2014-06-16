f56d048e25ddff522d659897cb17dfc4e0d22fc6
squaredinfinity <jarek@squaredinfinity.com>
2014-06-16 17:42:47 +0100

    EnumToValues converter added

---
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Converters/EnumToValuesConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using SquaredInfinity.Foundation.Extensions;
+
+namespace SquaredInfinity.Foundation.Presentation.Converters
+{
+    public class EnumToValuesConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            var en = value as Enum;
+
+            var enumValues = Enum.GetValues(en.GetType());
+
+            return enumValues;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
