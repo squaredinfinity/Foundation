932727639c651ce793b8aa2ccc2671deb4bf8717
squaredinfinity <jarek@squaredinfinity.com>
2014-07-11 18:20:14 +0100

    object.extensions - TryConvert() added

--- Foundation/Sources/Common/Foundation.Common/FrameworkExtensions/System/Object.extensions.cs
+++ Foundation/Sources/Common/Foundation.Common/FrameworkExtensions/System/Object.extensions.cs
@@ -1,6 +1,7 @@
 ﻿using SquaredInfinity.Foundation.Types.Mapping;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -116,6 +117,68 @@ namespace SquaredInfinity.Foundation.Extensions
         public static WeakEventHandlerPrototype<TSource> CreateWeakEventHandler<TSource>(this TSource source)
         {
             return new WeakEventHandlerPrototype<TSource>(source);
+        }
+
+        public static bool TryConvert(this object obj, Type targetType, out object result)
+        {
+            result = null;
+
+            if (obj != null)
+            {
+                var objType = obj.GetType();
+
+                if (objType.IsTypeEquivalentTo(targetType, treatNullableAsEquivalent: true, treatBaseTypesAsEquivalent: true, treatImplementedInterfacesAsEquivalent: true))
+                {
+                    result = obj;
+                    return true;
+                }
+            }
+
+            if(targetType.IsEnum)
+            {
+                if (obj is string)
+                {
+                    result = Enum.Parse(targetType, obj as string);
+                }
+                else
+                {
+                    result = Enum.ToObject(targetType, obj);
+                }
+
+                return true;
+            }
+
+            // todo: test if conversion enum -> string, enum -> int etc are supported
+
+            if (obj is IConvertible)
+            {
+                result = System.Convert.ChangeType(obj, targetType);
+                return true;
+            }
+
+            var converter = TypeDescriptor.GetConverter(obj);
+            if(converter != null && converter.CanConvertTo(targetType))
+            {
+                result = converter.ConvertTo(obj, targetType);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static object Convert(this object obj, Type targetType)
+        {
+            var result = (object)null;
+
+            if (TryConvert(obj, targetType, out result))
+                return result;
+
+            throw new InvalidOperationException("Unable to convert object to type {0}".FormatWith(targetType.FullName));
+        }
+
+        public static TTarget Convert<TTarget>(this object obj)
+        {
+            return (TTarget)Convert(obj, typeof(TTarget));
         }
     }
 }
