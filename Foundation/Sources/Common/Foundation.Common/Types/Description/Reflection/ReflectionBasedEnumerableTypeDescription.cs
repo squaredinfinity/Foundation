bfca493ac9f86c9adc3c138a2023dc18c1f226bf
squaredinfinity <jarek@squaredinfinity.com>
2014-07-25 17:04:13 +0100

    missing file added

---
+++ Foundation/Sources/Common/Foundation.Common/Types/Description/Reflection/ReflectionBasedEnumerableTypeDescription.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Types.Description.Reflection
+{
+    public class ReflectionBasedEnumerableTypeDescription : ReflectionBasedTypeDescription, IEnumerableTypeDescription
+    {
+
+        IReadOnlyList<Type> _compatibleItemTypes;
+        public IReadOnlyList<Type> CompatibleItemTypes
+        {
+            get { return _compatibleItemTypes; }
+            set { _compatibleItemTypes = value; }
+        }
+
+        Type _defaultConcreteItemType;
+        public Type DefaultConcreteItemType
+        {
+            get { return _defaultConcreteItemType; }
+            set { _defaultConcreteItemType = value; }
+        }
+
+
+        public bool CanSetCapacity
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public void SetCapacity(object obj, int capacity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
