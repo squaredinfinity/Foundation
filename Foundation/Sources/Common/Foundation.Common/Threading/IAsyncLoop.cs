8143cc841a105048632683477ed1dd5060aa76a1
squaredinfinity <jarek@squaredinfinity.com>
2014-08-29 14:50:06 +0100

    AsyncLoop - fixed memory leak issue where loop would not end if owner instance is GC collected (due to loop being referenced by the running task)

--- Foundation/Sources/Common/Foundation.Common/Threading/IAsyncLoop.cs
+++ Foundation/Sources/Common/Foundation.Common/Threading/IAsyncLoop.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SquaredInfinity.Foundation.Threading
 {
     public interface IAsyncLoop
     {
-        TimeSpan LoopIterationInterval { get; set; }
+        void Start(TimeSpan loopIterationDelay, CancellationToken cancellationToken);
+        //TimeSpan LoopIterationDelay { get; set; }
 
-        void Cancel();
+        //void Cancel();
     }
 }
