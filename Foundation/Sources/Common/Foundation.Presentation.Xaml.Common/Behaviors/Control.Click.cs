c98141cb86e002629357409b84f3f70e38b4e9e0
squaredinfinity <jarek@squaredinfinity.com>
2014-09-30 17:27:35 +0100

    XAML | ContentTemplateSelector.RefreshTriggerBinding behavior added

--- Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Behaviors/Control.Click.cs
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Behaviors/Control.Click.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,45 @@ using System.Windows.Input;
 
 namespace SquaredInfinity.Foundation.Presentation.Behaviors
 {
+    public class ContentTemplateSelector
+    {
+        #region RefreshTriggerBinding
+
+        public static void SetRefreshTriggerBinding(ContentPresenter element, bool value)
+        {
+            element.SetValue(RefreshTriggerBindingProperty, value);
+        }
+
+        public static object GetRefreshTriggerBinding(ContentPresenter element)
+        {
+            return (object)element.GetValue(RefreshTriggerBindingProperty);
+        }
+
+        public static readonly DependencyProperty RefreshTriggerBindingProperty =
+            DependencyProperty.RegisterAttached(
+            "RefreshTriggerBinding",
+            typeof(object),
+            typeof(ContentTemplateSelector),
+            new PropertyMetadata(null, OnRefreshTriggerBindingChanged));
+
+        static void OnRefreshTriggerBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var c = d as ContentPresenter;
+
+            if (c == null)
+                return;
+
+            var oldSelector = c.ContentTemplateSelector;
+
+            c.ContentTemplateSelector = null;
+
+            c.ContentTemplateSelector = oldSelector;
+        }
+
+
+        #endregion
+    }
+
     public class Click
     {
         #region Command
