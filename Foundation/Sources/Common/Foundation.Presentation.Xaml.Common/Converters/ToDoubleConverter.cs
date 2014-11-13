21aa0185bc6ccff34d8fa2ba4ca031e5ce9c8fe4
Squared Infinity <jarek@squaredinfinity.com>
2014-11-13 21:05:17 +0000

    Converters.ToDouble added

---
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Converters/ToDoubleConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using SquaredInfinity.Foundation.Extensions;
+
+namespace SquaredInfinity.Foundation.Presentation.Converters
+{
+    public class ToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            var nullableUnderlyingType = Nullable.GetUnderlyingType(value.GetType());
+
+            if(nullableUnderlyingType != null)
+                return (double)System.Convert.ToDouble(nullableUnderlyingType);
+
+            return (double)System.Convert.ToDouble(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
