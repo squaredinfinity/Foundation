07021262ca7b725bcf2b149d63c5d60746ec4d9f
Squared Infinity <jarek@squaredinfinity.com>
2014-11-18 22:07:18 +0000

    Type Extensions | IsNullabe will now return true if type is not a value type

--- Foundation/Sources/Common/Foundation.Common/FrameworkExtensions/System/Type.extensions.cs
+++ Foundation/Sources/Common/Foundation.Common/FrameworkExtensions/System/Type.extensions.cs
@@ -137,6 +137,9 @@ namespace SquaredInfinity.Foundation.Extensions
 
         public static bool IsNullable(this Type type)
         {
+            if (!type.IsValueType)
+                return true;
+
             if (Nullable.GetUnderlyingType(type) == null)
                 return false;
 
