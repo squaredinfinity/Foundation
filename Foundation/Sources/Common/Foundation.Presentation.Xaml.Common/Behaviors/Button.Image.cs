87da9f3873da7b9a927257c03985dc0dc3afa11a
Squared Infinity <jarek@squaredinfinity.com>
2015-02-26 20:26:31 +0000

    fixed namespace

--- Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Behaviors/Button.Image.cs
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Behaviors/Button.Image.cs
@@ -6,7 +6,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace  xxx.Foundation.Presentation.Behaviors
+namespace SquaredInfinity.Foundation.Presentation.Behaviors
 {
     public static partial class ButtonBehaviors
     {
