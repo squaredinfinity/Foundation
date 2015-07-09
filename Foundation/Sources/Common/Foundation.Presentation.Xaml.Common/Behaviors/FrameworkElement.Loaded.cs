5d5950a718c690ec7baf2a2609d11e0efef3290f
squaredinfinity <jarek@squaredinfinity.com>
2015-07-09 20:39:11 +0100

    FrameworkElement Loaded and Unloaded behaviors added

---
+++ Foundation/Sources/Common/Foundation.Presentation.Xaml.Common/Behaviors/FrameworkElement.Loaded.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SquaredInfinity.Foundation.Presentation.Behaviors
+{
+    public class Loaded
+    {
+        #region CommandParameters
+
+        public static void SetCommandParameter(FrameworkElement element, object value)
+        {
+            element.SetValue(CommandParameterProperty, value);
+        }
+
+        public static object GetCommandParameter(FrameworkElement element)
+        {
+            return (object)element.GetValue(CommandParameterProperty);
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached(
+            "CommandParameter",
+            typeof(object),
+            typeof(Loaded),
+            new PropertyMetadata(null));
+
+        #endregion
+
+        #region Command
+
+        public static void SetCommand(FrameworkElement element, ICommand value)
+        {
+            element.SetValue(CommandProperty, value);
+        }
+
+        public static ICommand GetCommand(FrameworkElement element)
+        {
+            return (ICommand)element.GetValue(CommandProperty);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached(
+            "Command",
+            typeof(ICommand),
+            typeof(Loaded),
+            new PropertyMetadata(null, OnCommandChanged));
+
+        static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var fe = d as FrameworkElement;
+
+            if (fe == null)
+                return;
+
+            if ((ICommand)e.NewValue != null)
+            {
+                fe.Loaded += c_Loaded;
+            }
+            else
+            {
+                fe.Loaded -= c_Loaded;
+            }
+        }
+
+        static void c_Loaded(object sender, RoutedEventArgs e)
+        {
+            var fe = sender as FrameworkElement;
+
+            if (fe == null)
+                return;
+
+            var command = GetCommand(fe);
+
+            var parameter = GetCommandParameter(fe);
+
+            if (command != null && command.CanExecute(parameter))
+            {
+                command.Execute(parameter);
+            }
+
+            e.Handled = true;
+        }
+
+        #endregion
+    }
+}
