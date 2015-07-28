9eed415121ad610168ce7626d51f87a99d538640
squaredinfinity <jarek@squaredinfinity.com>
2015-07-28 17:48:41 +0100

    added chace deplyoment project

---
+++ Foundation/Deployment/NuGet.Foundation/src/Shared/Foundation.Shared/Threading/ReaderWriterLockSlimEx.UpgradeableReadLockAcquisition.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Threading
+{
+    public partial class ReaderWriterLockSlimEx
+    {
+        class UpgradeableReadLockAcquisition : IUpgradeableReadLockAcquisition
+        {
+            ReaderWriterLockSlimEx Owner;
+
+            public UpgradeableReadLockAcquisition(ReaderWriterLockSlimEx owner)
+            {
+                this.Owner = owner;
+            }
+
+            public IReadLockAcquisition DowngradeToReadLock()
+            {
+                Owner.InternalLock.EnterReadLock();
+
+                return new ReadLockAcquisition(Owner);
+            }
+
+            public bool TryDowngradeToReadLock(TimeSpan timeout, out IReadLockAcquisition readLockAcquisition)
+            {
+                if(Owner.InternalLock.TryEnterReadLock(timeout.Milliseconds))
+                {
+                    readLockAcquisition = new ReadLockAcquisition(Owner);
+
+                    return true;
+                }
+                else
+                {
+                    readLockAcquisition = null;
+                    return false;
+                }
+            }
+
+            public IWriteLockAcquisition UpgradeToWriteLock()
+            {
+                Owner.InternalLock.EnterWriteLock();
+
+                //Owner.InternalLock.ExitUpgradeableReadLock();
+
+                return new WriteLockAcquisition(Owner);
+            }
+
+            public bool TryUpgradeToWriteLock(TimeSpan timeout, out IWriteLockAcquisition writeLockAcquisition)
+            {
+                throw new NotImplementedException();
+            }
+
+
+            public void Dispose()
+            {
+                if (Owner.InternalLock.IsUpgradeableReadLockHeld)
+                    Owner.InternalLock.ExitUpgradeableReadLock();
+            }
+        }
+    }
+}
--- Foundation/Sources/Common/Foundation.Common/Types/Mapping/MemberMatching/MemberMatchCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SquaredInfinity.Foundation.Types.Mapping.MemberMatching
-{
-    public class MemberMatchCollection : List<IMemberMatch> { }
-}
