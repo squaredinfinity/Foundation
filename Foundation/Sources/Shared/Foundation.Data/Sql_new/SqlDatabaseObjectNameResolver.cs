bba3d8e41d78f40fcdad6e2688c7e51c6ea70f23
squaredinfinity <jarek@squaredinfinity.com>
2015-08-25 09:32:41 +0100

    added missing files

---
+++ Foundation/Sources/Shared/Foundation.Data/Sql_new/SqlDatabaseObjectNameResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Data.Sql
+{
+    public class SqlDatabaseObjectNameResolver : IDatabaseObjectNameResolver
+    {
+        SqlDataAccessService _dataAccessService;
+        protected SqlDataAccessService DataAccessService
+        {
+            get { return _dataAccessService; }
+            private set { _dataAccessService = value; }
+        }
+
+        public SqlDatabaseObjectNameResolver(SqlDataAccessService dataAccessService)
+        {
+            this.DataAccessService = dataAccessService;
+        }
+
+        public virtual string GetActualStoredProcedureOrFunctionName(string storedProcedureOrFunctionName)
+        {
+            return storedProcedureOrFunctionName;
+        }
+    }
+}
