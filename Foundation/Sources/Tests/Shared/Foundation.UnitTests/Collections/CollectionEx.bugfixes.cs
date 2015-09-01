b59ad2c358b182f4448fa281b6ee7617efc7f7f5
squaredinfinity <jarek@squaredinfinity.com>
2015-09-01 17:56:12 +0100

    bug fix test added

---
+++ Foundation/Sources/Tests/Shared/Foundation.UnitTests/Collections/CollectionEx.bugfixes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Collections
+{
+	[TestClass]
+    public partial class CollectionExTests__BugFixes
+    {
+        [TestMethod]
+        public void BUG__001__CountAmbiguity()
+        {
+            // when casted to ICollectionEx<>, using Count fails build due to
+            // Error	Ambiguity between 'ICollection<>.Count' and 'IReadOnlyCollection<>.Count'
+
+            var col = new CollectionEx<int>();
+
+            var icol = col as ICollectionEx<int>;
+
+            Assert.AreEqual(icol.Count, 0);
+        }
+    }
+}
