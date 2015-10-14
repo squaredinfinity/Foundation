1f61193394c818a1ce62d8bdde6bd432bf33b503
squaredinfinity <jarek@squaredinfinity.com>
2015-10-14 13:22:17 +0100

    added ISerialzier interface

---
+++ Foundation/Sources/Shared/Foundation/ISerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredInfinity.Foundation
+{
+    public interface ISerialzier
+    {
+        SerializedDataInfo Serialize<T>(T obj);
+        T Deserialize<T>(SerializedDataInfo data);
+    }
+}
---
+++ Foundation/Sources/Shared/Foundation/SerializedDataInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredInfinity.Foundation
+{
+    public class SerializedDataInfo
+    {
+        public readonly byte[] Bytes;
+        public readonly Encoding Encoding;
+
+        public SerializedDataInfo(byte[] bytes, Encoding encoding)
+        {
+            this.Bytes = bytes;
+            this.Encoding = encoding;
+        }
+    }
+}
