12b6727b1b78276d3aef5ab39717a4eb21131b15
squaredinfinity <jarek@squaredinfinity.com>
2015-11-10 10:12:55 +0000

    Decimal extensions | ToStringNoTrailingZeros added

---
+++ Foundation/Sources/Shared/Foundation/FrameworkExtensions/System/Decimal.extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SquaredInfinity.Foundation.Extensions
+{
+    /// <summary>
+    /// Extensions of a Decimal type.
+    /// </summary>
+    public static partial class DecimalExtensions
+    {
+        public static string ToStringNoTrailingZeros(this decimal d)
+        {
+            return (d / 1.000000000000000000000000000000000m).ToString();
+        }
+    }
+}
