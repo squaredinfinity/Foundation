c28d5badd74127deddb7b0d2fc024283f8de0e3a
squaredinfinity <jarek@squaredinfinity.com>
2015-11-16 09:34:19 +0000

    SqlConnectionFactory added

---
+++ Foundation/Sources/Shared/Foundation.Data/SqlServer/SqlConnectionFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SquaredInfinity.Foundation.Data.SqlServer
+{
+    public class SqlConnectionFactory : ConnectionFactory<SqlConnection>
+    {
+        public SqlConnectionFactory()
+            : base("<not set>")
+        { }
+
+        string ConnectionString { get; set; }
+
+        public override void ChangeConnectionString(string newConnectionString)
+        {
+            ConnectionString = newConnectionString;
+        }
+
+        public override SqlConnection GetNewConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+    }
+}
