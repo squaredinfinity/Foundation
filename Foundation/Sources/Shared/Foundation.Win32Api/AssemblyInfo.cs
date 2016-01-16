5df30657248d3c25009c3f768ab077463e662c6b
Squared Infinity <jarek@squaredinfinity.com>
2016-01-16 20:43:48 +0000

    version up, deplyed to nuget.org

--- Foundation/Sources/Shared/Foundation.Win32Api/AssemblyInfo.cs
+++ Foundation/Sources/Shared/Foundation.Win32Api/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.7.21")]
-[assembly: AssemblyFileVersion("1.1.7.21")]
-[assembly: AssemblyInformationalVersion("1.1.7-beta021")]
+[assembly: AssemblyVersion("1.1.7.22")]
+[assembly: AssemblyFileVersion("1.1.7.22")]
+[assembly: AssemblyInformationalVersion("1.1.7-beta022")]
