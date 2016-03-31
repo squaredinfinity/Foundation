516911f8c4dacc5f5ed8b92edec35ee7482dd12c
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-03-31 23:05:41 +0100

    version up

--- Foundation/Sources/Shared/Foundation.Experimental/AssemblyInfo.cs
+++ Foundation/Sources/Shared/Foundation.Experimental/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.1.7.71")]
-[assembly: AssemblyFileVersion("1.1.7.71")]
-[assembly: AssemblyInformationalVersion("1.1.7-beta071")]
+[assembly: AssemblyVersion("1.1.7.80")]
+[assembly: AssemblyFileVersion("1.1.7.80")]
+[assembly: AssemblyInformationalVersion("1.1.7-beta080")]
