58b2c0605b40716be664e7aeeacaa7353dd4c4ab
squaredinfinity <jarek@squaredinfinity.com>
2016-04-04 19:00:39 +0100

    Statistics Min and Max added

---
+++ Foundation/Sources/Core/Foundation.Maths.Core/Statistics/Max.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Maths.Statistics
+{
+    public static class Max
+    {
+        public static double Calculate(IReadOnlyList<double> samples, bool ignoreNaNs)
+        {
+            var max = double.NaN;
+
+            for (int i = 0; i < samples.Count; i++)
+            {
+                var x = samples[i];
+
+                if (double.IsNaN(x))
+                    continue;
+
+                if (max < x || double.IsNaN(max))
+                    max = x;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Foundation/Sources/Core/Foundation.Maths.Core/Statistics/Min.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquaredInfinity.Foundation.Maths.Statistics
+{
+    public static class Min
+    {
+        public static double Calculate(IReadOnlyList<double> samples, bool ignoreNaNs)
+        {
+            var min = double.NaN;
+
+            for (int i = 0; i < samples.Count; i++)
+            {
+                var x = samples[i];
+
+                if (double.IsNaN(x))
+                    continue;
+
+                if (min > x || double.IsNaN(min))
+                    min = x;
+            }
+
+            return min;
+        }
+    }
+}
