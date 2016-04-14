b30fd5f539a9a42a266bab4dbe760f7cc84ebef5
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-04-14 16:16:21 +0100

    version up

--- Foundation/Sources/Shared/Internal/AssemblyInfo.experimental.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.experimental.shared.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 #if EXPERIMENTAL
 
-[assembly: AssemblyVersion("1.1.10.5")]
-[assembly: AssemblyFileVersion("1.1.10.5")]
-[assembly: AssemblyInformationalVersion("1.1.10-beta005")]
+[assembly: AssemblyVersion("1.1.10.6")]
+[assembly: AssemblyFileVersion("1.1.10.6")]
+[assembly: AssemblyInformationalVersion("1.1.10-beta006")]
 
 #endif
