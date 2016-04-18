345677ceddea4fadd526737047863a7207337cb0
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-04-18 18:43:27 +0100

    version up

--- Foundation/Sources/Shared/Internal/AssemblyInfo.graphics.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.graphics.shared.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 #if GRAPHICS
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.1.1")]
+[assembly: AssemblyFileVersion("1.2.1.1")]
+[assembly: AssemblyInformationalVersion("1.2.1-beta001")]
 
 #endif
--- Foundation/Sources/Shared/Internal/AssemblyInfo.presentation.xaml.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.presentation.xaml.shared.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 #if PRESENTATION_XAML
 
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.1.1")]
+[assembly: AssemblyFileVersion("1.2.1.1")]
+[assembly: AssemblyInformationalVersion("1.2.1-beta001")]
 
 #endif
