2e1ede2f8cd17784d21020dd6c8a7becab3ffc9a
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-06-27 11:51:14 +0100

    default style moved to Generic.Xaml

---
+++ Foundation/Sources/Shared/Foundation.Experimental/XamlResources.cs
@@ -0,0 +1,20 @@
+﻿using SquaredInfinity.Foundation.Presentation.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace SquaredInfinity.Foundation
+{
+    [XamlResourcesProviderMetadata(ImportOrder = XamlResources.ImportOrder)]
+    public class XamlResources : IXamlResourcesProvider
+    {
+        // Import Order is higher than Foundation.Presentation Import Order (on which resources from this assembly may depend)
+        public const uint ImportOrder = SquaredInfinity.Foundation.Presentation.XamlResources.ImportOrder + 1;
+
+        public IEnumerable<ResourceDictionary> LoadResources()
+        {
+            yield return ResourcesManager.LoadCompiledResourceDictionaryFromThisAssembly(@"Themes/Generic.xaml");
+        }
+    }
+}
