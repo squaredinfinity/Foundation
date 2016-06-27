399c97f9c0577557939188c2d92ac733d94e03ff
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-06-27 13:23:47 +0100

    AdaptiveSelector | removed border

--- Foundation/Sources/Shared/Internal/AssemblyInfo.experimental.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.experimental.shared.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 #if EXPERIMENTAL
 
-[assembly: AssemblyVersion("1.3.6.2")]
-[assembly: AssemblyFileVersion("1.3.6.2")]
-[assembly: AssemblyInformationalVersion("1.3.6-beta002")]
+[assembly: AssemblyVersion("1.3.6.3")]
+[assembly: AssemblyFileVersion("1.3.6.3")]
+[assembly: AssemblyInformationalVersion("1.3.6-beta003")]
 
 #endif
