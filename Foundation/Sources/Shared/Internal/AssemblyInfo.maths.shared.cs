52d58853f4c12c79a6b56cb8a9c2f5b97cf06b80
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-06-27 17:51:41 +0100

    version up

--- Foundation/Sources/Shared/Internal/AssemblyInfo.maths.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.maths.shared.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 #if MATHS
 
-[assembly: AssemblyVersion("1.3.6.0")]
-[assembly: AssemblyFileVersion("1.3.6.0")]
-[assembly: AssemblyInformationalVersion("1.3.6.0")]
+[assembly: AssemblyVersion("1.3.7.0")]
+[assembly: AssemblyFileVersion("1.3.7.0")]
+[assembly: AssemblyInformationalVersion("1.3.7.0")]
 
 #endif
