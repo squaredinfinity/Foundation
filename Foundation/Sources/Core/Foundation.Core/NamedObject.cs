3a816e27c7fa1ecb4f209f4916beb61bfb916ceb
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-06-29 10:55:42 +0100

    Named Object added Extensions namespace fixed

---
+++ Foundation/Sources/Core/Foundation.Core/NamedObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredInfinity.Foundation
+{
+    public class NamedObject
+    {
+        public string Name { get; private set; }
+        public object Object { get; private set; }
+
+        public NamedObject(string name, object obj)
+        {
+            this.Name = name;
+            this.Object = obj;
+        }
+    }
+}
