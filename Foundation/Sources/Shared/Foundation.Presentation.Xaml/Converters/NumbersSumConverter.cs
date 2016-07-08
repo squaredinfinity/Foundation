84eb895a2b17151c07dbed5f848201a19fc7c6c3
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-07-08 15:29:50 +0100

    NumbersSumConverter | when Overflow happens, return DP.UnsetValue instead of exception

--- Foundation/Sources/Shared/Foundation.Presentation.Xaml/Converters/NumbersSumConverter.cs
+++ Foundation/Sources/Shared/Foundation.Presentation.Xaml/Converters/NumbersSumConverter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using SquaredInfinity.Foundation.Extensions;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Windows.Data;
+using System.Windows;
 
 namespace SquaredInfinity.Foundation.Presentation.Converters
 {
@@ -10,24 +12,30 @@ namespace SquaredInfinity.Foundation.Presentation.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var sum = 0.0m;
-
-            var addend = 0.0m;
-
-            for (int i = 0; i < values.Length; i++)
+            try
             {
-                var raw_addend = values[i] as IConvertible;
+                var sum = 0.0m;
 
+                var addend = 0.0m;
 
-                if (raw_addend == null)
-                    continue;
+                for (int i = 0; i < values.Length; i++)
+                {
+                    var raw_addend = values[i] as IConvertible;
 
-                addend = System.Convert.ToDecimal(raw_addend);
+                    if (raw_addend == null)
+                        continue;
 
-                sum += addend;
+                    addend = System.Convert.ToDecimal(raw_addend);
+
+                    sum += addend;
+                }
+                
+                return sum;
             }
-
-            return sum;
+            catch(OverflowException)
+            {
+                return DependencyProperty.UnsetValue;
+            }
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
