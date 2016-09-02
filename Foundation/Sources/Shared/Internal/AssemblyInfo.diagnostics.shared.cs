a1b9584ad88e178245613a03bb74459a312d95f9
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-09-02 19:58:41 +0100

    version up

--- Foundation/Sources/Shared/Internal/AssemblyInfo.diagnostics.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.diagnostics.shared.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 #if DIAGNOSTICS
 
-[assembly: AssemblyVersion("1.3.19.0")]
-[assembly: AssemblyFileVersion("1.3.19.0")]
-[assembly: AssemblyInformationalVersion("1.3.19.0")]
+[assembly: AssemblyVersion("1.3.20.0")]
+[assembly: AssemblyFileVersion("1.3.20.0")]
+[assembly: AssemblyInformationalVersion("1.3.20.0")]
 
 #endif
--- Foundation/Sources/Shared/Internal/AssemblyInfo.graphics.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.graphics.shared.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 #if GRAPHICS
 
-[assembly: AssemblyVersion("1.3.19.0")]
-[assembly: AssemblyFileVersion("1.3.19.0")]
-[assembly: AssemblyInformationalVersion("1.3.19.0")]
+[assembly: AssemblyVersion("1.3.20.0")]
+[assembly: AssemblyFileVersion("1.3.20.0")]
+[assembly: AssemblyInformationalVersion("1.3.20.0")]
 
 #endif
