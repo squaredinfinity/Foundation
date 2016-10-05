7cda55b52c976f99810ad42439d5284e0d877130
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-10-05 14:37:58 +0100

    Removed Maths.Core from Maths.Statistics output

--- Foundation/Sources/Shared/Internal/AssemblyInfo.maths.statistics.shared.cs
+++ Foundation/Sources/Shared/Internal/AssemblyInfo.maths.statistics.shared.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 
 #if MATHS_STATISTICS
 
-[assembly: AssemblyVersion("1.4.5.0")]
-[assembly: AssemblyFileVersion("1.4.5.0")]
-[assembly: AssemblyInformationalVersion("1.4.5.0")]
+[assembly: AssemblyVersion("1.4.6.0")]
+[assembly: AssemblyFileVersion("1.4.6.0")]
+[assembly: AssemblyInformationalVersion("1.4.6.0")]
 
 #endif
