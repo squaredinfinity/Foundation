50eec9c0dd71b078f4e37efa7a430eb457e29cb5
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2016-10-10 11:26:55 +0100

    PixelCanvas | .DrawRectangle() fixed for conditions

--- Foundation/Sources/Core/Foundation.Graphics.Core/Drawing/PixelCanvas.DrawLine.cs
+++ Foundation/Sources/Core/Foundation.Graphics.Core/Drawing/PixelCanvas.DrawLine.cs
@@ -28,14 +28,24 @@ namespace SquaredInfinity.Foundation.Graphics.Drawing
             DrawRectangle((int)x1_d, (int)y1_d, (int)(x2_d -x1_d), (int)(y2_d - y1_d), color);
         }
 
+        /// <summary>
+        /// Draws a rectangle at specified x and y coordinates.
+        /// This method does not perform any checks and will throw if rectangle is outside of canvas bounds.
+        /// If this is the case use an overload that accepts Bounds
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="color"></param>
         public void DrawRectangle(int x, int y, int width, int height, int color)
         {
             var x2 = x + width;
             var y2 = y + height;
 
-            for (; x <= x2; x++)
+            for (; x < x2; x++)
             {
-                for (int _y = y; _y <= y2; _y++)
+                for (int _y = y; _y < y2; _y++)
                 {
                     this[x, _y] = color;
                 }
