bd1b5a488ef1beabcde07480acb88f6e5d4bbd54
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2017-03-14 10:54:00 +0000

    ...

---
+++ Foundation/Sources/Internal/AssemblyInfo.shared.cs
@@ -0,0 +1,67 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if FOUNDATION_COMPARERS
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_DATETIME
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_DISPOSABLES
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_EXTENSIONS
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_PRIMITIVES
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_RETRYPOLICY
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_BULKUPDATES
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
+
+#if FOUNDATION_COMPONENTMODEL
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
+
+#endif
