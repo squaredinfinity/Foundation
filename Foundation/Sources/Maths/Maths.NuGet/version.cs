1788ad7ebfd214b0289db6308c1a57eb1186897b
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2017-05-03 08:36:32 +0100

    maths.dotnet45 and maths.extensions.dotnet45 added

--- Foundation/Sources/Maths/Maths.NuGet/version.cs
+++ Foundation/Sources/Maths/Maths.NuGet/version.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("Maths")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
