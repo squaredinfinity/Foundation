2aa720f46c1f05555364d132fb21b20cf4d0f57a
jarek <jarek@squaredinfinity.com>
2017-05-28 18:51:29 +0100

    nugets updated

--- Foundation/Sources/Maths/Maths.NuGet/version.cs
+++ Foundation/Sources/Maths/Maths.NuGet/version.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("Maths")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.10.0")]
+[assembly: AssemblyFileVersion("2.0.11.0")]
