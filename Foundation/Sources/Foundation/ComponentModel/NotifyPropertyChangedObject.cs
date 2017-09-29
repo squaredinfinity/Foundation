32ff7d11d5077a6ca135c42880c4028b5983d6a1
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2017-09-29 08:06:12 +0100

    ComponentModel converted tto .net standard

--- Foundation/Sources/Foundation/ComponentModel/NotifyPropertyChangedObject.cs
+++ Foundation/Sources/Foundation/ComponentModel/NotifyPropertyChangedObject.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 using System.Text;
 using System.Threading;
 
