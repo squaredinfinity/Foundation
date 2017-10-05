bd0a1ed237ba99ae79031ecbe2537a83ea8a4fd8
jarek@squaredinfinity.com <jarek@squaredinfinity.com>
2017-10-05 20:07:46 +0100

    IDummyDisposableProvider added

---
+++ Foundation/Sources/Foundation/Disposables/IDummyDisposableProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquaredInfinity.Disposables
+{
+    /// <summary>
+    /// Defines a method to create a dummy disposable.
+    /// Dummy disposable does nothing when .Dispose() is called.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IDummyDisposableProvider<T>
+        where T : IDisposable
+    {
+        /// <summary>
+        /// Returns an instance of this object which implements dummy .Dispose().
+        /// Calling .Dispose() on that instance will not result with instance being disposed.
+        /// </summary>
+        /// <returns></returns>
+        T AsDummyDisposable();
+    }
+}
